511a9ebcb82e0e79fc79aef1130207a0181a6348
ccait <360897160@qq.com>
2020-04-15 00:39:55 -0500

    ProfilePage Dumb Version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import CitiesPage from './components/layout/Cities';
 import EditMapPage from './components/layout/EditMap';
-import ProfilePage from './components/layout/Profile';
+import ProfilePage from './components/layout/ProfilePage/ProfilePage';
 import SignInPage from './components/auth/Signin';
 import SignUpPage from './components/auth/Signup';
 
-class App extends Component {
+class App extends Component{
   render() {
+    const PublicRoute = ({ component: Component , ...rest})=>{
+      return(
+          <Route {...rest}  component={(props)=>(
+              <div>
+                <Navbar/> {/* Navbar ALWAYS VISIBLE */}
+                <Component {...props} />
+              </div>)}
+    />
+    )}
+    const AdminRoute = ({ component: Component, ...rest }) => {
+      return (
+          <Route
+              {...rest}
+              component={(props)=> (<Component {...props} />)}
+          />
+      );
+    };
+
     return (
       <BrowserRouter>
         <div className="App">
-          <Navbar />
           <Switch>
-            <Route exact path='/' component={CitiesPage} />
-            <Route path='/profile/:id' component={ProfilePage} />
-            <Route path='/map/:id' component={EditMapPage}/>
-            <Route path='/signin' component={SignInPage} />
-            <Route path='/signup' component={SignUpPage} />
+            <PublicRoute exact path='/' component={CitiesPage} />
+            <PublicRoute path='/map/:id' component={EditMapPage}/>
+            <PublicRoute path='/signin' component={SignInPage} />
+            <PublicRoute path='/signup' component={SignUpPage} />
+            <AdminRoute path='/profile' component={ProfilePage} />
           </Switch>
         </div>
       </BrowserRouter>
--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -5,7 +5,7 @@ const SignedInLinks = () => {
   return (
     <ul>
       <li>If user signedIn</li>
-      <li><NavLink to='/profile/3'>Profile</NavLink></li>
+      <li><NavLink to='/profile/user'>Profile</NavLink></li>
       <li><NavLink to='/'>Log Out</NavLink></li>
     </ul>
   )
