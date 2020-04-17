9da2fb9bc5cd6206446be1cf631b62a1d12c39d4
yuqi wang <jeffwanjeff@yuqis-MacBook-Pro.local>
2020-04-17 16:17:38 -0700

    new toplist text added

--- src/components/Dashboard/CityGrid.js
+++ src/components/Dashboard/CityGrid.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
@@ -28,7 +28,7 @@ const useStyles = makeStyles((theme) => ({
     },
     gridList: {
         flexWrap: 'wrap',
-        height: 600,
+        height: 625,
         width: 300,
         // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
         transform: 'translateZ(0)',
@@ -50,40 +50,34 @@ export default function CityGrid(props) {
         // }
         return data.map((point) => (
             <GridListTile key={point.id}>
-                <img onClick={()=>props.setCity(point.name + ", " + point.country)} src={point.images[0]} alt=" "/>} />
+                <img onClick={() => props.setCity(point.name + ", " + point.country)} src={point.images[0]} alt=" "/>}
+                />
                 <GridListTileBar
                     title={point.name}
                     classes={{
                         root: classes.titleBar,
                         title: classes.title,
                     }}
-                    // actionIcon={
-                    //     {/*<IconButton>*/}
-                    //     {/*    <FavoriteIcon style={iconStyle} />*/}
-                    //     {/*</IconButton>*/}
-                    // }
                 />
             </GridListTile>
         ))
     }
-console.log(props.data)
+    console.log(props.data)
     return (
+
         <div className="TopList">
+            <div className="TopList-text">
+                Alpha Cities
+            </div>
             <List
                 component="div"
                 className={classes.root}
             >
-                <ListItem button onClick={handleClick}>
-                    <ListItemIcon>
-                        <CollectionsIcon color="secondary"/>
-                    </ListItemIcon>
-                    <ListItemText primary="Alpha Cities" />
-                </ListItem>
-                    <div className={classes.gridroot}>
-                        <GridList cellHeight={200} spacing={1} className={classes.gridList} cols={1}>
-                            {props.data.default && createGridList(props.data.default)}
-                        </GridList>
-                    </div>
+                <div className={classes.gridroot}>
+                    <GridList cellHeight={200} spacing={1} className={classes.gridList} cols={1}>
+                        {props.data.default && createGridList(props.data.default)}
+                    </GridList>
+                </div>
             </List>
         </div>
     );
