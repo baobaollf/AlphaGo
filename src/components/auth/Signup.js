6207c25a9f98f05088941f7cb54a1cb35daf236f
peter yang <peteryang@peters-MacBook-Pro.local>
2020-04-19 15:59:14 -0400

    finish adjustment of Mainpage and signup signin page

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import "./Style.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Form } from 'react-bootstrap';
+import { width, height } from '@material-ui/system';
 
 
 class SignUpPage extends Component {
@@ -25,43 +26,43 @@ class SignUpPage extends Component {
     console.log(this.state);
   }
 
+  
   render() {
-    // const backgroundPic = {
-    //   // backgroundImage: "url(" + this.props.image + ")
-    //   backgroundImage: `url(${background})`
-    // }
-
+    
     return (
-      <div>
+
+      <div className="signuppage">
         {/* <div className="background" 
-          // style={{  
-          //   backgroundImage: background,
-          //   backgroundPosition: 'center',
-          //   backgroundSize: 'cover',
-          //   backgroundRepeat: 'no-repeat'
-          // }}>
+          style={{  
+            backgroundImage: backgroundPic,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat'
+          }}> 
           </div> */}
-        <Form className="form" onSubmit={this.handleSubmit}>
-          <Form.Group size="lg">
-            <Form.Label className="label">Username</Form.Label>
-            <Form.Control id="username" type="username" placeholder="Username" onChange={this.handleChange}/>
-          </Form.Group>
-          <Form.Group size="lg">
-            <Form.Label className="label">Email address</Form.Label>
-            <Form.Control id="email" type="emails" placeholder="Email" onChange={this.handleChange}/>
-          </Form.Group>
-          <Form.Group size="lg">
-            <Form.Label className="label">Password</Form.Label>
-            <Form.Control id="password" type="password" placeholder="Password" onChange={this.handleChange}/>
-          </Form.Group>
-          
-          <Form.Text className="text-muted">
-            By signing up, you agree to all user terms.
-          </Form.Text>
-          <Button className="button" size="sm" variant="warning" type="submit" >
-            <p className="signup">Sign up</p>
-          </Button>
-        </Form>
+
+        <div className="signupform">
+          <Form className="form" onSubmit={this.handleSubmit}>
+            <Form.Group size="lg">
+              <Form.Label className="label">Username</Form.Label>
+              <Form.Control id="username" type="username" placeholder="Username" onChange={this.handleChange}/>
+            </Form.Group>
+            <Form.Group size="lg">
+              <Form.Label className="label">Email address</Form.Label>
+              <Form.Control id="email" type="emails" placeholder="Email" onChange={this.handleChange}/>
+            </Form.Group>
+            <Form.Group size="lg">
+              <Form.Label className="label">Password</Form.Label>
+              <Form.Control id="password" type="password" placeholder="Password" onChange={this.handleChange}/>
+            </Form.Group>
+            <Form.Text className="text-muted">
+              By signing up, you agree to all user terms.
+            </Form.Text>
+            <Button className="button" size="sm" variant="warning" type="submit" >
+              <p className="signup">Sign up</p>
+            </Button>
+          </Form>
+        </div>
       </div>
 
     )
