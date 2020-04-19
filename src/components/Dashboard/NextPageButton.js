ac3ab81ed37042ad465731371650fbcef4911877
baobaollf <55776365+baobaollf@users.noreply.github.com>
2020-04-19 19:44:23 -0400

    fixed mainpage data transfer error

--- src/components/Dashboard/NextPageButton.js
+++ src/components/Dashboard/NextPageButton.js
@@ -6,43 +6,51 @@ import {Button} from "antd";
 class NextPageButton extends Component {
     cityList = (data) => {
         const cityName = [];
-        for(var i = 0; i < data.length; i++) {
+        for (var i = 0; i < data.length; i++) {
             cityName.push(data[i].name + ", " + data[i].country);
         }
         return cityName
     }
-    
-    render() {
-        for(let i = 0; i < this.cityList(this.props.data.default).length; i++) {
-            if (this.props.city === this.cityList(this.props.data.default)[i]) {
-                return (
-                    <div className="next-page">
-                        <NavLink to={{
-                            pathname: '/map/3',
-                            details: {
-                                city: this.props.city.split(',')[0],
-                                coordinates: this.props.coordinates,
-                                days: this.props.day
-                            }
-                        }}>
-                            <Button type="primary" icon="thunderbolt">
-                                Let's Go
-                            </Button>
-                        </NavLink>
-                    </div>
-                );   
-            } else {
-                return (
-                    <div className="next-page">
-                        <Button type="primary" icon="thunderbolt">
-                            Please enter city
-                        </Button>
-                    </div>
-                );
-            }  
+
+    checkEnter = (input) => {
+        for (let i = 0; i < this.cityList(this.props.data.default).length; i++) {
+            if (input === this.cityList(this.props.data.default)[i]) {
+                return true;
+            }
         }
-        
-        
+        return false;
+    }
+
+    render() {
+        if (this.checkEnter(this.props.city)) {
+            console.log(this.props.city)
+            return (
+                <div className="next-page">
+                    <NavLink to={{
+                        pathname: '/map/3',
+                        details: {
+                            city: this.props.city.split(',')[0],
+                            coordinates: this.props.coordinates,
+                            days: this.props.day
+                        }
+                    }}>
+                        <Button type="primary" icon="thunderbolt">
+                            Let's Go
+                        </Button>
+                    </NavLink>
+                </div>
+            );
+        } else {
+            return (
+                <div className="next-page">
+                    <Button type="primary" icon="thunderbolt">
+                        Please enter city
+                    </Button>
+                </div>
+            );
+        }
+
+
     }
 }
 
